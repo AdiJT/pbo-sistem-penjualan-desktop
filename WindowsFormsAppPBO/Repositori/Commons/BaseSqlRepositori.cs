1eb8d7b774166041704a4abd5939595d8d67a502
Adi Juanito Taklal <aditaklal@gmail.com>
2023-12-19 12:28:52 +0800

    Tambah IBaseRepositori dan BaseSqlRepositori

---
+++ WindowsFormsAppPBO/Repositori/Commons/BaseSqlRepositori.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsAppPBO.Repositori.Commons
+{
+    public abstract class BaseSqlRepositori<T> : IBaseRepositori<T> where T : class
+    {
+        protected readonly AppDbContext dbContext;
+
+        public BaseSqlRepositori(AppDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public virtual T Add(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual T Delete(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual T Get(string id)
+        {
+            var result = dbContext.Set<T>().Find(id);
+
+            return result;
+        }
+
+        public virtual IList<T> GetAll()
+        {
+            var result = dbContext.Set<T>()
+                .AsNoTracking().ToList();
+
+            return result;
+        }
+
+        public virtual T Update(T item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WindowsFormsAppPBO/Repositori/Commons/IBaseRepositori.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsAppPBO.Repositori.Commons
+{
+    public interface IBaseRepositori<T> where T : class
+    {
+        IList<T> GetAll();
+        T Get(string id);
+        T Add(T item);
+        T Update(T item);
+        T Delete(string id);
+    }
+}
