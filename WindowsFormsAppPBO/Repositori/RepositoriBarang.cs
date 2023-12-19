ef6ffa6d05fc0101f02793c0cec7665995882fe6
Adi Juanito Taklal <aditaklal@gmail.com>
2023-12-19 13:04:54 +0800

    Override Update repositori barang

--- WindowsFormsAppPBO/Repositori/RepositoriBarang.cs
+++ WindowsFormsAppPBO/Repositori/RepositoriBarang.cs
@@ -17,10 +17,42 @@ namespace WindowsFormsAppPBO.Repositori
             
         }
 
+        public override IList<Barang> GetAll()
+        {
+            var result = dbContext.TblBarang.Include(b => b.Kategori)
+                .Include(b => b.DaftarDetailBarang).ToList();
+
+            return result;
+        }
+
         public override Barang Get(string id)
         {
             var result = dbContext.TblBarang.Include(b => b.Kategori)
                 .Include(b => b.DaftarDetailBarang).FirstOrDefault(b => b.KodeBarang == id);
+
+            return result;
+        }
+
+        public override Barang Update(string id, Barang item)
+        {
+            var result = dbContext.TblBarang.Find(id);
+            if (result != null)
+            {
+                result.NamaBarang = item.NamaBarang;
+                result.IdKategori = item.IdKategori;
+
+                var listDetailLama = result.DaftarDetailBarang.ToList();
+                foreach (var detail in listDetailLama)
+                {
+                    dbContext.TblDetailBarang.Attach(detail);
+                    dbContext.TblDetailBarang.Remove(detail);
+                }
+                dbContext.SaveChanges();
+
+                result.DaftarDetailBarang = item.DaftarDetailBarang.ToList();
+
+                dbContext.SaveChanges();
+            }
 
             return result;
         }
